943bfe8823bd1590afe882221ca9bd8097029a8b
frappe <frappe@ubuntu-2204.linuxvmimages.local>
2024-12-11 16:50:47 +0530

    meta data issue

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='ocr_app',  # Name of the app
-    version='0.0.1',        # Your app version
+    version='1.0.0',        # Your app version
     description='OCR App for Frappe',  # A short description
-    author='Your Name',     # Replace with your name
-    author_email='your_email@example.com',  # Replace with your email
+    author='lmc',     # Replace with your name
+    author_email='sanketshelar2002@gmail.com',  # Replace with your email
     packages=find_packages(),  # Automatically find all packages in your app
     include_package_data=True,  # Include non-Python files (e.g., images, templates, static)
     zip_safe=False,            # Set to False if your app is not safe to be zipped
