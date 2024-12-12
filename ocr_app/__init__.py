ed9ae2f7444f3372ee318eeb1c5ff00a283d1107
frappe <frappe@ubuntu-2204.linuxvmimages.local>
2024-12-12 16:55:40 +0530

    config

--- ocr_app/__init__.py
+++ ocr_app/__init__.py
@@ -1,1 +1,4 @@
 from . import api
+def get_module(modulename):
+    print("Trying to load module:", modulename)  # Add this line
+    return importlib.import_module(modulename)
--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='ocr_app',  # Name of the app
-    version='1.0.0',        # Your app version
+    version='0.0.1',        # Your app version
     description='OCR App for Frappe',  # A short description
     author='lmc',     # Replace with your name
     author_email='sanketshelar2002@gmail.com',  # Replace with your email
     packages=find_packages(),  # Automatically find all packages in your app
     include_package_data=True,  # Include non-Python files (e.g., images, templates, static)
     zip_safe=False,            # Set to False if your app is not safe to be zipped
-    install_requires=[         # List of dependencies
+    install_requires=[  
+        'frappe'       # List of dependencies
       'pytesseract',          # If using Tesseract OCR, for example
         'requests',             # Example of another common dependency
         'Pillow',
