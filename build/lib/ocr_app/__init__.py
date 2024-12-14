ef8efcfa4e81118a61239ddcbf26200719c95e73
frappe <frappe@ubuntu-2204.linuxvmimages.local>
2024-12-14 11:04:30 +0530

    config changes

---
+++ build/lib/ocr_app/__init__.py
@@ -0,0 +1,2 @@
+# from _future_ import unicode_literals
+_version_ = "0.0.1"
--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     name='ocr_app',  # Name of the app
     version=version,        # Your app version
     description='OCR App for Frappe',  # A short description
+    url="https://github.com/Aditya123learner/ocr_app",
     author='lmc',     # Replace with your name
     author_email='sanketshelar2002@gmail.com',  # Replace with your email
     packages=find_packages(),  # Automatically find all packages in your app
